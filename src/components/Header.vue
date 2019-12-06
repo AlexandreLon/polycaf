b9159bfc6ba44d8b855de541a2664828b9ae93db
daminor26 <daminor26@gmail.com>
2019-12-06 01:32:30 +0100

    bug

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -28,7 +28,7 @@
             </div>
         </div>
     </div>
-    <CategoryDetails v-if="detailToShowChild != null" :detailToShow="detailToShowChild" />
+    <CategoryDetails ref="categoryDetails" v-if="detailToShowChild != null" :detailToShow="detailToShowChild" />
   </div>
 </template>
 
@@ -42,9 +42,22 @@
           detailToShowChild: null
         }
       },
+      mounted : function(){
+        console.log(this.$refs.categoryDetails)
+      },
       methods: {
         clickOn: function (clicked) {
           this.detailToShowChild = clicked;
+
+          console.log(this.$refs)
+          console.log(this.$refs.categoryDetails)
+          console.log(this.$refs['categoryDetails'])
+/*
+          window.scroll({
+            behavior: 'smooth',
+            top: categoryDetails.offsetTop
+          });
+          */
         }
       }
     }
