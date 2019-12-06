ffc4f5c8b7df14192aaaaa5a06eeb9beb2eafc4b
daminor26 <daminor26@gmail.com>
2019-12-06 02:56:03 +0100

    mao

--- src/components/CategoryDetailsCaf.vue
+++ src/components/CategoryDetailsCaf.vue
@@ -1,7 +1,7 @@
 <template>
-    <div>
-      Caf
-    </div>
+  <div style="width: 700px;position: relative;">
+    <iframe width="700" height="440" src="https://maps.google.com/maps?width=700&amp;height=440&amp;hl=en&amp;q=caf&amp;ie=UTF8&amp;t=&amp;z=10&amp;iwloc=B&amp;output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+  </div>
 </template>
 
 <script>
--- src/components/CategoryDetailsCrous.vue
+++ src/components/CategoryDetailsCrous.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
-
+    <div style="width: 700px;position: relative;">
+      <iframe width="700" height="440" src="https://maps.google.com/maps?width=700&amp;height=440&amp;hl=en&amp;q=crous+(Mon%20crous%20le%20plus%20proche)&amp;ie=UTF8&amp;t=&amp;z=10&amp;iwloc=B&amp;output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+    </div>
   </div>
+
 </template>
 
 <script>
